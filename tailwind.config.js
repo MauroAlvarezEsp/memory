cfd65af5c822ae45f4a391232ffc81e2c8254092
Mauro Alvarez Espinosa <mauro.alvarez@scotiabank.com.mx>
2025-02-01 17:40:59 -0600

    animations and root styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flip: {
+          '0%': { transform: 'rotateY(0deg)' },
+          '100%': { transform: 'rotateY(180deg)' },
+        }
+      },
+      animation: {
+        flip: 'flip 0.2s forwards',
+      }
+    },
   },
   plugins: [],
 }
