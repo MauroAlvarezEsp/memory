907bc034e2a281258eb6731d0f4def7a713ad149
Mauro Alvarez Espinosa <mauro.alvarez@scotiabank.com.mx>
2025-02-02 10:56:32 -0600

    add dynamic lenguage detection

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,11 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
-    // the translations
-    // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
     resources: {
         en: {
             translation: {
@@ -35,9 +34,7 @@ i18n
                 "label.finished": "Terminaste exitosamente el juego"
             }
         }
-    },
-    lng: "en", 
-    fallbackLng: "en",
+    }, 
     interpolation: {
       escapeValue: false
     }
