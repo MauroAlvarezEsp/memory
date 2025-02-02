db8bf36a0201862c2fbc42540210abf6a5c8b743
Mauro Alvarez Espinosa <mauro.alvarez@scotiabank.com.mx>
2025-02-01 18:35:36 -0600

    congrats component

---
+++ src/modules/memory/components/congrats.component.tsx
@@ -0,0 +1,22 @@
+import { useTranslation } from "react-i18next";
+import { getUserName } from "../../../commons/utils/utils"
+
+const Congrats = (props: {restart: Function}) => {
+    const userName = getUserName();
+    const { t } =  useTranslation();
+
+    return(
+        <div className="text-center p-32 bg-blue-950 text-white shine">
+            <h1 className="text-lg">{userName}</h1>
+            <h1 className="text-4xl mt-5 font-bold">{t('label.congratulations')}</h1>
+            <h1 className="text-md text-gray-300 mb-5">{t('label.finished')}</h1>
+            <button 
+                onClick={() => props.restart()}
+                className="text-md bg-white text-black rounded-full px-3 py-1 text-sm font-semibold">
+                {t('label.repeat')}
+            </button>
+        </div>
+    )
+}
+
+export default Congrats
