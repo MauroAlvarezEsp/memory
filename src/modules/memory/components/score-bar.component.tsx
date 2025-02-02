7d4c45923ad03fdca75f0a7211d6c74dc1fb772f
Mauro Alvarez Espinosa <mauro.alvarez@scotiabank.com.mx>
2025-02-02 11:15:37 -0600

    size adjustment

--- src/modules/memory/components/score-bar.component.tsx
+++ src/modules/memory/components/score-bar.component.tsx
@@ -5,7 +5,7 @@ export const ScoreBar = (props: {score: ScoreType}) => {
     const { t } = useTranslation();
 
     return (
-        <div className="flex w-[45%] lg:w-[30%] m-auto justify-around pt-5 pb-1">
+        <div className="flex w-[70%] sm:w-[40%] lg:w-[30%] m-auto justify-around pt-5 pb-1">
             <h1 className="text-xl font-semibold">{t('label.hits')} {props.score.hits}</h1>
             <h1 className="text-xl font-semibold">{t('label.errors')} {props.score.errors}</h1>
         </div>
